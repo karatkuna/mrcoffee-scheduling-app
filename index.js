fde4ee0721a24e70f54b299c052e26ec54e2248a
karatkuna <karat.kuna@gmail.com>
2021-11-01 17:54:48 +1100

    Set up index.js file

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const morgan = require('morgan')
+const ejs = require('ejs')
+
+// const homeRouter = require('./routes/home')
+// const usersRouter = require('./routes/users')
+// const schedulesRouter = require('./routes/schedules')
+// const errorRouter = require('./routes/error')
+
+const PORT = process.env.PORT || 3000
+
+app.express()
+
+// BODY PARSER
+app.use(express.json())
+app.use(urlencoded({extended: true}))
+
+// MIDDLEWARE: LOGGER
+app.use(morgan('dev'))
+
+// USING TEMPLATE ENGINE
+app.set('view engine', 'ejs')
+
+// SET STATIC FOLDER
+app.use(express.static('public'))
+
+// ROUTES
+app.use('/users', usersRouter)
+app.use('/schedules', schedulesRouter)
+app.use('/', homeRouter)
+app.use('*', errorRouter)
+
+app.listen(PORT, () => {
+  console.log(`LISTENING AT http://localhost:${PORT}`)
+})
